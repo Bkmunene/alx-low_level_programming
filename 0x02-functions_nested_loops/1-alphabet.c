946e30bcf6551a47eb174c930eeff38ef747d164
Benson Munene <bkmunene@gmail.com>
2023-02-21 08:42:37 -0800

    0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,22 @@
-#include "stdio.h"
+#include "main.h"
 
 /**
- * print_alphabet - print alphabet
+ * main - Entry point
  *
- * Return: void
+ * Return: Always 0 (Success)
  */
-
-void print_alphabet(void)
+int main(void)
 {
-	char letter = 'a';
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-		letter++;
-	}
-	_putchar('\n');
+	return (0);
 }
+
