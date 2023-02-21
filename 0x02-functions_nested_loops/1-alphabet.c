c3c2cea317c54dd99f56c06acc2ae7caffb458ac
Benson Munene <bkmunene@gmail.com>
2023-02-21 09:33:10 -0800

    lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
+#include "main.h"
 
+/**
+  * print_alphabet - program that prints alphabets in lowercase
+  */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+}
