a481bea397cc03e211605ce18c2d6dd3d76d049e
Benson Munene <bkmunene@gmail.com>
2023-03-12 01:39:19 -0800

    dEL

---
+++ 0x00-hello_world/103-benson_kariuki.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/** main - a c function that prints ken's name
+ * Return: this function will return 0 because mr ken is a non integer
+ */
+
+int main(void)
+{
+	printf("Kennedy waweru\n");
+	Return (0);
+}
